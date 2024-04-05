f1b3542ac3bd0678b7750b2df2a84ede5aaa7122
Ciprian Vlad <ciprianvlad000@gmail.com>
2024-04-05 02:20:33 +0200

    12344

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import './main.scss';
+import './styles/main.scss';
 import SearchResults from './Components/SearchResults';
 import BookCard from './Components/BookCard';
 
